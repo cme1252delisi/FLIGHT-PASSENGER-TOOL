fccb21f2326c0e40ef81f65dbd435b8c8d309d61
M3hm3t4li <smehmetali2005@gmail.com>
2026-04-09 18:11:23 +0300

    teferruatlar

---
+++ flight passenger tool/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <libxml/parser.h> //baylar bu iki arkadaş bu şekil kullanılcak terminalden compile oluyor da vs code u ayarlayamadım hocanın yaptığı gibi siz ayarlarsınız belki.
+#include <libxml/tree.h>
+
+int main(){
+    printf("%s","iranı rahat bırakın");
+
+
+    return 0;
+}
